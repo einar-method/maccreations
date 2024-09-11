6b5f3203f949fdf2aa36c345cc7158075e80e491
Traveling Bard <92512445+einar-method@users.noreply.github.com>
2024-09-11 15:44:45 +0900

    a: converted blog post content to handle JSON format from Instant db. b: brought in the navigation header from old page. c: removed some uneeded files and made a function to delete the Instant logo. d: lots of CSS tweaks. e: full blog post viewed as an overlay. f: started the section tab, using work from ez tools.

--- old.js
+++ old.js
@@ -187,3 +187,13 @@ function submitForm(event) {
     input.value = '';
   }
 }
+
+// export function setupCounter(element) {
+//   let counter = 0
+//   const setCounter = (count) => {
+//     counter = count
+//     element.innerHTML = `count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
+// }
