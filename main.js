5566556a1033e4a69838f89ff7f6bff91f0346ae
Traveling Bard <92512445+einar-method@users.noreply.github.com>
2024-09-13 17:01:57 +0900

    a: seperated the nav buttons from the app. b: created JS driven styling for nav buttons that determine the active tab for the whole website - which also inserts the html we need. c: brought in old landing home page for the home tab. d: made mock pages for portfolio and merch to ensure the tab functionality works.
    TODO:
    a: clean up uneeded code that is commented out.
    b: create a card system for portfolio and merch pages that displays my projects (portfolio) and my itch products as well as buy me a coffee (merch).
    c: input remaining blog data into INdeed (there may be changes required to display the articles by date created irl - not when they were added to Indeed.

--- main.js
+++ main.js
@@ -36,6 +36,18 @@ const app = document.getElementById('app');
 //app.style.cssText = styles.container;
 
 function render(data) {
+  document.getElementById("navHeader").innerHTML = renderNavHeader();
+
+  window.navButtons = new NavButtons();
+  window.navButtons.applyAllStyles();
+
+  document.querySelectorAll('.nav__header_btns').forEach(button => {
+    button.addEventListener('click', function() {
+      const buttonId = this.getAttribute('id');
+      navBtnClicked(buttonId, data);
+    });
+  });
+
   app.innerHTML = '';
 
   console.log(data)
@@ -44,26 +56,30 @@ function render(data) {
 
   console.log(blog)
 
-  const { todos } = data;
+  // const { todos } = data;
 
-  console.log(todos);
+  // console.log(todos);
 
-  const containerHTML = `
-    ${renderHeader()}
-    <main class="blog__page">
-      ${makeBlogSections()}
-      ${blogPost(blog)}
-    </main>
-  `;
+  // const containerHTML = `
+  //   <main class="blog__page">
+  //     ${makeBlogSections()}
+  //     ${blogPost(blog)}
+  //   </main>
+  // `;
 
-  app.innerHTML = containerHTML;
+  //let containerHTML = checkNavBtn(blog);
+  checkNavBtn(blog);
 
-  document.querySelectorAll('.blog__post').forEach(article => {
-    article.addEventListener('click', function() {
-      const postId = this.getAttribute('id');
-      handleArticleClick(blog, postId);
-    });
-  });
+  //app.innerHTML = containerHTML;
+
+  //app.innerHTML = makeBlogPage(blog);
+
+  // document.querySelectorAll('.blog__post').forEach(article => {
+  //   article.addEventListener('click', function() {
+  //     const postId = this.getAttribute('id');
+  //     handleArticleClick(blog, postId);
+  //   });
+  // });
 
   // Attach event listeners
   // document.querySelector('.toggle-all')?.addEventListener('click', () => toggleAllTodos(todos));
@@ -81,10 +97,132 @@ function renderError(errorMessage) {
   `;
 }
 
-function blogPost(blogPosts) {
+function makeHomePage(blank) {
+  return `
+    <dialog id="tip"><strong></strong> <span id="tip-txt"></span></dialog>
+    <main class="home__page">
+      <section class="welcome__text">
+        <figure>
+          <img class="cover__image" src="Assests/em-anime.webp" alt="An AI generated paitning of the CJ, aka EM, in a fantasy setting." />
+          </figure>
+        <!-- <h1>Æinär's Library</h1> -->
+        <p class="about-blurb">
+          Aspiring game developer; WIP web developer; RPG hobbyist. 
+        </p>
+      </section>
+
+      <section>
+        <br />
+        <!-- Add font awesome icons -->
+        <div class="outer">
+          <div class="icons">
+            <a
+              href="https://aka-em.itch.io/"
+              target="_blank"
+              class="fa-brands fa-itch-io"
+            ></a>
+          </div>
+    
+          <div class="icons">
+            <a
+              href="https://github.com/einar-method"
+              target="_blank"
+              class="fa fa-github"
+            ></a>
+          </div>
+          <div class="icons">
+            <a
+              href="https://travelingbardgames.substack.com/"
+              target="_blank"
+              class="fa fa-rss"
+            ></a>
+          </div>
+          <!-- <div class="icons">
+            <a
+              href="https://www.youtube.com/channel/UCTVQYIFLY7d5E9m57tGRKXw"
+              target="_blank"
+              class="fa fa-youtube"
+              id="yt"
+            ></a>
+          </div> -->
+          <div class="icons">
+            <a
+              href="https://open.spotify.com/track/1GGLQhjZSB6h0Qkfu2OvAf?si=4cafaec035cb4ed6"
+              class="fa-brands fa-spotify"
+              target="_blank"
+            ></a>
+          </div>
+          <div class="icons">
+            <a
+              href="https://ordes.netlify.app/"
+              class="fa-solid fa-globe"
+              target="_blank"
+            ></a>
+          </div>
+          <div class="icons">
+            <a
+              href="https://buymeacoffee.com/em.bmc"
+              class="fa-solid fa-mug-hot"
+              target="_blank"
+            ></a>
+          </div>
+        </div>
+        <br />
+      </section>
+
+      <section class="janken__section">
+        <p class="green__text">Let's play janken!</p>
+        <div class="mini-game">
+          <div class="game-btns">
+            <button class="janken__buttons" id="rock">rock</button>
+            <button class="janken__buttons" id="paper">paper</button>
+            <button class="janken__buttons" id="scissors">scissors</button>
+          </div>
+        </div>
+      </section>
+    </main>
+  `;
+};
+
+function makePortfolioPage(dataIn) {
+  return `
+    <main class="blog__page">
+      <p>This is the portfolio page.</p>
+    </main>
+  `;
+};
+
+function makeMerchPage(dataIn) {
+  return `
+    <main class="blog__page">
+      <p>This is the merch page.</p>
+    </main>
+  `;
+}
+
+function makeBlogPage(blogIn) {
+  return `
+    <main class="blog__page">
+      ${makeBlogSections()}
+      ${blogPost(blogIn)}
+    </main>
+  `;
+};
+
+function setUpBlogInteraction(dataIn) {
+  document.querySelectorAll('.blog__post').forEach(article => {
+    article.addEventListener('click', function() {
+      const postId = this.getAttribute('id');
+      handleArticleClick(dataIn.blog, postId);
+    });
+  });
+}
+
+function blogPost(dataIn) {
+  //console.log(dataIn.blog)
   return `
     <section class="blog__section">
-        ${blogPosts.map(post => `
+        ${dataIn.blog.map(post => `
           <article class="blog__post" id="blogID${post.id}">
             <h2 class="blog__header">${post.title}</h2>
             <h3 class="blog__subheader">${post.subtitle}</h3>
@@ -144,11 +282,19 @@ function makeBlogSections() {
 function renderHeader() {
   return `
     <header id="header">
-        <button class="h-btns" id="home" onclick="window.location.href='#';">home</button>
-        <button class="h-btns" id="ezd6" onclick="window.location.href='EZD6/ez-index.html';">portfolio</button>
-        <button class="h-btns" id="itch" onclick="window.location.href='itch.html';">merch</button>
-        <button class="h-btns" id="substack" onclick="window.location.href='substack.html';">blog</button>
+        <button class="nav__header_btns" id="homeBtn">home</button>
+        <button class="nav__header_btns" id="portfolioBtn">portfolio</button>
+        <button class="nav__header_btns" id="merchBtn">merch</button>
+        <button class="nav__header_btns" id="blogBtn">blog</button>
     </header>
+  `
+}
+function renderNavHeader() {
+  return `
+    <button class="nav__header_btns" id="homeBtn">home</button>
+    <button class="nav__header_btns" id="portfolioBtn">portfolio</button>
+    <button class="nav__header_btns" id="merchBtn">merch</button>
+    <button class="nav__header_btns" id="blogBtn">blog</button>
   `
 }
 
@@ -315,6 +461,220 @@ function removeElementWithStyles() {
   });
 };
 document.addEventListener('DOMContentLoaded', function() {
-  // Call your function here after the DOM content is loaded
   removeElementWithStyles();
-});  
+});
+
+class NavButtons {
+  constructor() {
+    this.buttons = [
+      { name: "home", id: "homeBtn", isActive: true },
+      { name: "portfolio", id: "portfolioBtn", isActive: false },
+      { name: "merch", id: "merchBtn", isActive: false },
+      { name: "blog", id: "blogBtn", isActive: false }
+    ];
+
+    this.defaultStyle = `
+      color: var(--white-color);
+      background-color: var(--primary-color);
+      border: 0.1px solid var(--white-color);
+      border-radius: 0px;
+      padding: 5px 5px;
+      display: inline-block;
+      font-size: 18px;
+      letter-spacing: 1px;
+      cursor: pointer;
+      box-shadow: inset 0 0 0 0 var(--white-color);
+      -webkit-transition: ease-out 0.4s;
+      -moz-transition: ease-out 0.4s;
+      transition: ease-out 0.4s;
+      flex-grow: 1;
+    `;
+
+    this.activeStyle = `
+      color: var(--tirtary-color);
+      background-color: var(--primary-color);
+      border: 0.1px solid var(--white-color);
+      border-radius: 0px;
+      padding: 5px 5px;
+      display: inline-block;
+      font-size: 18px;
+      letter-spacing: 1px;
+      cursor: default;
+      box-shadow: inset 0 0 0 25px var(--secondary-color);
+      -webkit-transition: ease-out 0.4s;
+      -moz-transition: ease-out 0.4s;
+      transition: ease-out 0.4s;
+      flex-grow: 1;
+    `;
+
+    this.hoverStyle = `
+      color: var(--secondary-color);
+      background-color: var(--primary-color);
+      border: 0.1px solid var(--white-color);
+      border-radius: 0px;
+      padding: 5px 5px;
+      display: inline-block;
+      font-size: 18px;
+      letter-spacing: 1px;
+      cursor: pointer;
+      box-shadow: inset 0 0 0 25px var(--white-color);
+      -webkit-transition: ease-out 0.4s;
+      -moz-transition: ease-out 0.4s;
+      transition: ease-out 0.4s;
+      flex-grow: 1;
+    `
+  }
+
+  // Method to apply styles based on isActive
+  applyStyles(button) {
+    //console.error(button);
+    //console.log(button.isActive);
+    //console.log(button.id);
+    //console.log(document.getElementById(button.id));
+    const buttonElement = document.getElementById(button.id);
+    buttonElement.style = button.isActive ? this.activeStyle : this.defaultStyle;
+    
+    //buttonElement.setAttribute('style', button.isActive ? this.activeStyle : this.defaultStyle);
+    //buttonElement.setAttribute("setHover", "0");
+    //console.log(document.getElementById(button.id));
+
+    buttonElement.addEventListener('mouseover', () => {
+      if (!button.isActive) {
+        buttonElement.style = this.hoverStyle;
+      }
+    });
+    buttonElement.addEventListener('mouseout', () => {
+      buttonElement.style = button.isActive ? this.activeStyle : this.defaultStyle;
+    });
+
+    //document.styleSheets[0].insertRule(`#${button.id}:hover { background-color: red; }`, 1);
+  }
+
+  // Apply styles to all buttons
+  applyAllStyles() {
+    document.styleSheets[0].insertRule("#homeBtn:hover { background-color: red; }", 1);
+    this.buttons.forEach(button => this.applyStyles(button));
+  }
+}
+
+function navBtnClicked(buttonId, dataIn) {
+  //console.log(buttonId);
+  // Reset all buttons to default style
+  // window.navButtons.buttons.forEach(button => {
+  //   button.isActive = false;
+  //   window.navButtons.applyStyles(button);
+  // });
+  window.navButtons.buttons.forEach(button => {
+    if (button.id === buttonId) {
+      button.isActive = true;
+      //window.navButtons.applyStyles(button);
+      checkNavBtn(dataIn);
+    } else {
+      button.isActive = false;
+    }
+    window.navButtons.applyStyles(button);
+  });
+};
+
+function checkNavBtn(dataIn) {
+  const activeButton = window.navButtons.buttons.find(button => button.isActive === true);
+
+  if (activeButton) {
+    console.log(`The active button's id is: ${activeButton.id}`);
+    if (activeButton.id === "homeBtn") {
+      console.log("Home button is active");
+      app.innerHTML = makeHomePage();
+      setupJankenGame();
+    } else if (activeButton.id === "portfolioBtn") {
+      console.log("Portfolio button is active");
+      app.innerHTML = makePortfolioPage();
+    } else if (activeButton.id === "merchBtn") {
+      console.log("Merch button is active");
+      app.innerHTML = makeMerchPage();
+    } else if (activeButton.id === "blogBtn") {
+      console.log("Blog button is active");
+      app.innerHTML = makeBlogPage(dataIn);
+      setUpBlogInteraction(dataIn);
+    } else {
+      console.log("No active button found");
+    }
+  } else {
+    console.log('No active button found.');
+  }
+};
+
+
+// JANKEN GAME
+function setupJankenGame() {
+  const possibleChoices = document.querySelectorAll('button.janken__buttons');
+
+  possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+      const userChoice = e.target.id;
+      const computerChoice = generateComputerChoice();
+      const result = getResult(computerChoice, userChoice);
+      const msg = `You picked ${userChoice}, CJ picked ${computerChoice}.<br>${result}`
+      callTip(msg)
+  }))
+};
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    return randomNumber === 0 ? 'rock' : randomNumber === 1 ? 'scissors' : 'paper';
+};
+
+function getResult(computer, player) {
+    let result
+    if (computer === player) {
+        result = "It's a draw..."
+    }
+    if (computer === 'rock' && player === 'paper') {
+        result = "You win!"
+    }
+    if (computer === 'rock' && player === 'scissors') {
+        result = "Sorry, you lost :("
+    }
+    if (computer === 'paper' && player === 'scissors') {
+        result = "You win!"
+    }
+    if (computer === 'paper' && player === 'rock') {
+        result = "Sorry, you lost :("
+    }
+    if (computer === 'scissors' && player === 'paper') {
+        result = "Sorry, you lost :("
+    }
+    if (computer === 'scissors' && player === 'rock') {
+        result = "You win!"
+    }
+    return result
+}
+
+// DIALOG BOXES
+function fadeInElements(elementIds) {
+    requestAnimationFrame(function () {
+        elementIds.forEach(function (elementId) {
+        // Check the computed style to ensure the initial styles are applied
+        window.getComputedStyle(document.getElementById(elementId)).opacity;
+  
+        // Set opacity to 1 after the initial styles are applied
+        document.getElementById(elementId).style.opacity = 1;
+      });
+    });
+};
+
+function dialogFade(element, opacity) {
+    element.style.opacity = opacity;
+    element.style.transition = "none"; // Disable transition temporarily
+    requestAnimationFrame(() => {
+      element.style.transition = ""; // Re-enable transition
+    });
+};
+
+function callTip(txt) {
+    document.getElementById("tip-txt").innerHTML = txt;
+    document.getElementById("tip").open = true;
+    fadeInElements(["tip"])
+    setTimeout(() => {
+        document.getElementById("tip").open = false;
+        dialogFade(document.getElementById("tip"), 0)
+    }, 3500);
+};
